eaf20e0bdac25df3ab20612bc89f6cba9bde1765
coilwhine <hen.daniel47@gmail.com>
2024-02-26 13:09:49 +0200

    asd

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import "./Header.scss";
+import { MdPrivacyTip } from "react-icons/md";
+
 
 function Header(): JSX.Element {
     return (
         <div className="Header">
             <h1>Gepner Lab - Garmin Project</h1>
+            <Link className="privacy-icon" to={"/privacys-statement"}>
+                <MdPrivacyTip />
+            </Link>
         </div>
     );
 }
